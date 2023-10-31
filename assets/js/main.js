72e2e1aee90969798a6623ae5c3efcfcb13b87cd
abdul-aziz <tamimaziz2007@gmail.com>
2023-10-31 09:49:17 +0600

    testimonia file

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        loop:true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+      });
+});
