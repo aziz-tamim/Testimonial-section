5fc4630aa61706ba17e2321a60f4aeb34f83cfcb
abdul-aziz <tamimaziz2007@gmail.com>
2023-10-31 17:12:53 +0600

    all done

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,10 @@ $(document).ready(function () {
         slidesPerView: 4,
         spaceBetween: 30,
         loop:true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+          },
         pagination: {
           el: ".swiper-pagination",
           clickable: true,
